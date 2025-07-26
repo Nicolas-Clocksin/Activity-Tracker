55aad87d0694f185b4d26da80cd28ad5bfc30940
nicolas-clocksin-ASU <60439673+nicolas-clocksin-ASU@users.noreply.github.com>
2025-07-25 22:54:03 -0700

    fix: updated so correct date/format is shown. removed intialized instance of activities

--- activity-tracker/src/App.jsx
+++ activity-tracker/src/App.jsx
@@ -5,36 +5,12 @@ import Header from './Header'
 function App() {
   const timestamp = Date.now();
   const dateObject = new Date(timestamp);
-  const [activities, setActivities]= useState([
-    {
-        name: 'Activity Name',
-        day: new Date().getDay(),
-        year: new Date().getFullYear(),
-        month: new Date().getMonth() + 1,
-        time: dateObject.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
-        notes: 'notes about an activity'
-    },
-    {
-        name: 'Activity Name',
-        day: new Date().getDay(),
-        year: new Date().getFullYear(),
-        month: new Date().getMonth() + 1,
-        time: '05:00 PM',
-        notes: 'notes about an activity'
-    },
-    {
-        name: 'Activity Name',
-        day: 10,
-        year: 2025,
-        month: 3,
-        time: '05:00 PM',
-        notes: 'notes about an activity'
-    }
-]);
+  const [selectedDate, setSelectedDate] = useState(dateObject);
+  const [activities, setActivities]= useState([]);
   return(
     <div className='activity-tracker-container'>
-    <Header />
-    <ActivityList activities={activities} setActivities={setActivities}/>
+    <Header selectedDate = {selectedDate} currentDate={dateObject} />
+    <ActivityList activities={activities} setActivities={setActivities} />
     </div>
   );
 }
--- activity-tracker/src/Header.jsx
+++ activity-tracker/src/Header.jsx
@@ -1,12 +1,17 @@
-function Header(){
-    const currentDate = new Date(Date.now());
-    const currentYear = currentDate.getYear();
-    const currentMonth = currentDate.getMonth() + 1;
-    const currentDay = currentDate.getDate();
+function Header({selectedDate, currentDate}){
+    const currentYear = selectedDate.getFullYear();
+    const currentMonth = selectedDate.getMonth() + 1;
+    const currentDay = selectedDate.getDate();
+    console.log(selectedDate, currentDate);
     return(
     <header className="header">
+       { console.log(selectedDate === currentDate) }
+
         <h1>Activity Tracker</h1>
-        <p>Today is: {currentMonth} / {currentDay} / {currentYear} </p>
+        {selectedDate.getDate() == currentDate.getDate ? 
+         <p>Today is: {currentMonth} / {currentDay} / {currentYear} </p>
+       : <p> {currentMonth} / {currentDay} / {currentYear} </p>
+        }
     </header>
     );
 }
