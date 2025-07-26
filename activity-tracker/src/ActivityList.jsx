db33c6cb3deb522ca6ebba455b9b34ba57a62ee1
nicolas-clocksin-ASU <60439673+nicolas-clocksin-ASU@users.noreply.github.com>
2025-07-25 18:16:44 -0700

    feat: added ability to add new activities

--- activity-tracker/src/ActivityList.jsx
+++ activity-tracker/src/ActivityList.jsx
@@ -3,6 +3,14 @@ import React, { useState } from 'react'
 function ActivityList(){
     const timestamp = Date.now();
     const dateObject = new Date(timestamp);
+    const [activityName, setActivityName] = useState('');
+    const [activityDay, setActivityDay] = useState();
+    const [activityMonth, setActivityMonth] = useState();
+    const [activityYear, setActivityYear] = useState();
+    const [activityNote, setActivityNote] = useState('');
+    const [activityTime, setActivityTime] = useState();
+    const [activityDateTime, setActivityDateTime] = useState('');
+
     const [activities, setActivities]= useState([
         {
             name: 'Activity Name',
@@ -19,8 +27,52 @@ function ActivityList(){
             month: new Date().getMonth() + 1,
             time: '05:00 PM',
             notes: 'notes about an activity'
+        },
+        {
+            name: 'Activity Name',
+            day: 10,
+            year: 2025,
+            month: 3,
+            time: '05:00 PM',
+            notes: 'notes about an activity'
         }
     ]);
+    function updateActivityName(event){
+       setActivityName(event.target.value)
+    }
+    function updateDateTime(event){
+        const dayTimeSubmitted = event.target.value;
+        const date = new Date(dayTimeSubmitted);
+
+        setActivityDay(date.getDay());
+        setActivityMonth(date.getMonth()+1);
+        setActivityYear(date.getFullYear());
+        const time = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+        setActivityTime(time);
+        setActivityDateTime(event.target.value);
+    }
+    function updateNote(event){
+        setActivityNote(event.target.value);
+    }
+    function addActivity(){
+        console.log('add activity');
+        setActivities(
+            prev => ([...prev, 
+                {
+                    name: activityName,
+                    day: activityDay,
+                    time: activityTime,
+                    month: activityMonth,
+                    year: activityYear,
+                    notes: activityNote
+                }
+            ])
+        );
+        setActivityName('');
+        setActivityNote('');
+        setActivityTime('');
+        setActivityDateTime('');
+    }
     return(
         <div>
             <ul>
@@ -41,6 +93,12 @@ function ActivityList(){
                   )
                 }
             </ul>
+            <div className='add-activity'>
+                <input type="text" placeholder='Name of activity' value={activityName} onChange={updateActivityName}/>
+                <input id="time" value={activityDateTime} onChange={(event)=> updateDateTime(event)} type="datetime-local"/>
+                <textarea value={activityNote} onChange={(event) => updateNote(event)} placeholder='Add Notes for Activity'></textarea>
+            <button onClick={addActivity}>Add Activity</button>
+            </div>
         </div>
     )
 }
